51df0c1c5359359fe920c1c46653e98fbc420572
colingell <colingell@yahoo.co.uk>
2017-04-14 08:57:53 +0100

    Added Boilerplate

--- trepi-mobile-head.php
+++ trepi-mobile-head.php
@@ -1,20 +1,80 @@
 <?php
 
-/*
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://trepidation.com
+ * @since             1.0.0
+ * @package           trepi_mobile_head
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Trepidation Mobile Head Plugin
+ * Plugin URI:        http://trepidation.com
+ * Description:       A plugin by trepidation Creative for creating a mobile head.
+ * Version:           1.0.0
+ * Author:            Colin Gell
+ * Author URI:        http://trepidation.co.uk/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       Mobile-Head-for-Wordpress
+ * Domain Path:       /languages
+ */
 
-Plugin Name: Trepidation Mobile Head Plugin
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
-Plugin URI: http://trepidation.com
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+ */
+function activate_trepi_mobile_head() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	trepi_mobile_head_Activator::activate();
+}
 
-Description: A plugin by trepidation Creative for creating a mobile head
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_trepi_mobile_head() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	trepi_mobile_head_Deactivator::deactivate();
+}
 
-Author: Colin Gell
+register_activation_hook( __FILE__, 'activate_trepi_mobile_head' );
+register_deactivation_hook( __FILE__, 'deactivate_trepi_mobile_head' );
 
-Version: 1.0
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
 
-Author URI: http:/trepidation.com
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_trepi_mobile_head() {
 
-*/
+	$plugin = new trepi_mobile_head();
+	$plugin->run();
+
+}
+run_trepi_mobile_head();
+
+
 
 
 /* Add hook to after body */
